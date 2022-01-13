6b1aa0b3705da4a558458453ed3d5fcaac594b11
FarhanBinShafiq <farhan.shafiq@northsouth.edu>
2022-01-13 18:42:22 +0600

    react project

---
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDy7eX5VLWJKT3KT9QHbup7H1tOu39qWTc",
+    authDomain: "genius-car-78344.firebaseapp.com",
+    projectId: "genius-car-78344",
+    storageBucket: "genius-car-78344.appspot.com",
+    messagingSenderId: "843243341233",
+    appId: "1:843243341233:web:d76fba4d1f51ea28a52990"
+  };
+
+export default firebaseConfig;
